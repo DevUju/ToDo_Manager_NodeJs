9867009b4484109badae8beaae025444d5621c88
Obianuju1998 <agnesanosike@gmail.com>
2025-07-25 22:46:49 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var todoController = require("./controllers/todoController")
+
+var app = express();
+
+// Template Engine
+app.set("view engine", "ejs");
+
+// Static Files
+app.use(express.static("node-js-playlist/public"));
+
+// Fire Controller
+todoController(app)
+
+// Listen to Port
+localhost:3000/assets/styles.css
+
+app.listen(3000);
+
+console.log("You are listening to port 3000");
