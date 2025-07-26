fc2be20880b03ef82573c2059c9e9f8e848b5760
Obianuju1998 <agnesanosike@gmail.com>
2025-07-26 23:23:52 +0100

    db-test

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,34 @@
+var mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://agnesanosike:UjuAg123.@cluster0.mongodb.net/Todo_Manager")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("Connection error:", err));
+
+
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model("Todo", todoSchema);
+
+(async () => {
+    try {
+      const itemOne = new Todo({ item: "Get DE Fomm" });
+      await itemOne.save();
+      console.log("Item Saved");
+    } catch (err) {
+      console.error("Error saving item:", err);
+    }
+  })();
+
+    
+
 var bodyParser = require("body-parser");
+
 var data = [{item: "Build DJF Website"}, {item: "Get a DE Form"}, {item: "Secure an Internship in BE"}]
+
 var urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+
 
 module.exports = function(app){
     
